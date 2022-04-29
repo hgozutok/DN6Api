a7408d9d21db588618f996e69c294cce037fdb27
Huseyin GOZUTOK <hgozutok@gmail.com>
2022-04-29 12:42:35 +0300

    .

--- DN6Api/Program.cs
+++ DN6Api/Program.cs
@@ -21,6 +21,7 @@ var builder = WebApplication.CreateBuilder(args);
 // add services to DI container
 {
     var services = builder.Services;
+    //cors
     services.AddCors();
     services.AddControllers();
 
